52690e139d85907cd9d28e528f45fcbb928493ab
rupert <rupert@he360.com>
2022-06-21 12:49:03 +0000

    add retry

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from celery import Celery
 from flask import Flask, request
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 os.environ.setdefault('CELERY_CONFIG_MODULE', 'celery_config')
 
@@ -8,9 +11,12 @@ celery = Celery('app')
 celery.config_from_envvar('CELERY_CONFIG_MODULE')
 
 
-@celery.task
+@celery.task(autoretry_for=(Exception,), max_retries=3)
 def add(x, y):
+    logger.info('Adding {0} + {1}'.format(x, y))
     return x + y
+
+
 
 app = Flask(__name__)
 
